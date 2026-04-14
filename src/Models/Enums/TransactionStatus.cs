92ecebd684006ffad5a66fdd9df4caae4d69475b
micjaw310589 <idiotjuice165@gmail.com>
2026-04-14 23:41:08 +0200

    Delete src/Models

--- src/Models/Enums/TransactionStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace F1BettingGame.Models.Enums;
-
-public enum TransactionStatus
-{
-    Pending,
-    Completed,
-    Failed,
-    Reversed
-}
--- src/Models/Enums/UserBonusStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace F1BettingGame.Models.Enums;
-
-public enum UserBonusStatus
-{
-    Active,
-    Used,
-    Expired,
-    Cancelled
-}
